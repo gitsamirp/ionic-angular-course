8daf82a6bd43cd264a2299f38f0be0e186db9640
rdisamir <samirp123@hotmail.co.uk>
2019-09-20 22:10:23 +0100

    create pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,31 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { 
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule' 
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule',
+      }
+    ]
+  },
+  { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
+  { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
+  { path: 'search', loadChildren: './places/search/search.module#SearchPageModule' },
+  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
+  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
+  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
+  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
+  { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
+  { path: 'place-bookings', loadChildren: './places/offers/place-bookings/place-bookings.module#PlaceBookingsPageModule' },
+  { path: 'offer-bookings', loadChildren: './places/offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule' },
+  { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
+
 ];
 
 @NgModule({

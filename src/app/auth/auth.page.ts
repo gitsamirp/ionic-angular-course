ecaa9c855dd0a1db09e492ba272c0a54692182e9
rdisamir <samirp123@hotmail.co.uk>
2019-10-07 20:29:47 +0100

    login page

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -11,6 +11,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) {
 
@@ -35,7 +36,21 @@ export class AuthPage implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLogin) {
+      //Send request to login server
+    } else {
+      //send request to sign up
+    }
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 
 }
